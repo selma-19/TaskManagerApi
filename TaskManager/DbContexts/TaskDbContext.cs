87e634224ee1ab969f6c4aaad2005007986c32a9
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-07-16 22:56:23 +0100

    Creating task entity class & DbContext

---
+++ TaskManager/DbContexts/TaskDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.DbContexts
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KPFA88B\\\\MSSQLSERVER01;Database=CityInfoDB;Integrated ;Security=true;Encrypt=False;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .HasKey(t => t.Id);
+
+            // ... any additional configuration for your entities
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TaskManager/Entities/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Entities
+{
+    public class Task
+    {
+      [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+
+    }
+}
